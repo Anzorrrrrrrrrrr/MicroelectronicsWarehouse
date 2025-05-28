85e68cfbe0c8ad758ccef9f2941fbae3a1cbfa9a
bogdan.vasylevskyi@gmail.com <bogdan.vasylevskyi@gmail.com>
2025-05-28 16:37:09 +0300

    додано Data Access Layer

---
+++ Repositories/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using MicroelectronicsWarehouse.Entities;
+
+namespace MicroelectronicsWarehouse.Repositories.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<Category> Categories { get; }
+        IGenericRepository<Supplier> Suppliers { get; }
+        IGenericRepository<Component> Components { get; }
+
+        Task<int> CompleteAsync(); // SaveChanges
+    }
+}
---
+++ Repositories/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using MicroelectronicsWarehouse.Data;
+using MicroelectronicsWarehouse.Entities;
+using MicroelectronicsWarehouse.Repositories.Interfaces;
+
+namespace MicroelectronicsWarehouse.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public IGenericRepository<Category> Categories { get; private set; }
+        public IGenericRepository<Supplier> Suppliers { get; private set; }
+        public IGenericRepository<Component> Components { get; private set; }
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+            Categories = new GenericRepository<Category>(context);
+            Suppliers = new GenericRepository<Supplier>(context);
+            Components = new GenericRepository<Component>(context);
+        }
+
+        public async Task<int> CompleteAsync() => await _context.SaveChangesAsync();
+
+        public void Dispose() => _context.Dispose();
+    }
+}
